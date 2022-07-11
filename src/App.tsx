2218db34d8ec0bb9898e8b4ba1b04d4d2fdb45d5
Sanka Sanjeewa <sanka.s@sms.fidenz.com>
2022-07-11 16:17:17 +0530

    Add twin css
    removed App.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
+/** @jsxImportSource @emotion/react */
+import "twin.macro";
+import React from "react";
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div tw="flex justify-center">
+      <img src={logo} tw="h-20" alt="logo" />
     </div>
   );
 }
